c10483aa800f304ab979773e476ad52ffb8fbec3
Rahil Verma <rahilv99@gmail.com>
2025-05-10 12:20:27 -0400

    disable prefetch for supabase

--- src/webapp/lib/db/drizzle.ts
+++ src/webapp/lib/db/drizzle.ts
@@ -9,5 +9,5 @@ if (!process.env.POSTGRES_URL) {
   throw new Error('POSTGRES_URL environment variable is not set');
 }
 
-export const client = postgres(process.env.POSTGRES_URL);
+export const client = postgres(process.env.POSTGRES_URL, { prepare: false });
 export const db = drizzle(client, { schema });
